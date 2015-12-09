026ce01db5a82ff9b93bb301fee2ed3fd98a835c
Yen-Bo Huang <yenbohuang@hotmail.com>
2015-12-09 15:16:20 +0800

    add CRC and MD5 demo

--- commonDemo/src/main/java/org/yenbo/commonDemo/HashDemo.java
+++ commonDemo/src/main/java/org/yenbo/commonDemo/HashDemo.java
@@ -3,14 +3,25 @@ package org.yenbo.commonDemo;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.zip.CRC32;
 
 public class HashDemo {
 
-	public static byte[] getSha256Hash(byte[] input)
+	public static String ALGORITHM_SHA256 = "SHA-256";
+	public static String ALGORITHM_MD5 = "MD5";
+	
+	public static byte[] hash(String algorithm, byte[] input)
 	throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		
-	       MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	       MessageDigest digest = MessageDigest.getInstance(algorithm);
 	       digest.reset();
 	       return digest.digest(input);
-	 }
+	}
+	
+	public static long getCrc32(byte[] input) {
+		
+		CRC32 crc32 = new CRC32();
+		crc32.update(input);
+		return crc32.getValue();
+	}
 }
--- commonDemo/src/test/java/org/yenbo/commonDemo/test/HashDemoTest.java
+++ commonDemo/src/test/java/org/yenbo/commonDemo/test/HashDemoTest.java
@@ -1,6 +1,7 @@
 package org.yenbo.commonDemo.test;
 
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yenbo.commonDemo.HashDemo;
@@ -12,7 +13,13 @@ public class HashDemoTest {
 	public static void main(String[] args) {
 		
 		try {
-			log.info(Hex.encodeHexString(HashDemo.getSha256Hash("".getBytes("UTF-8"))));
+			//byte[] binary = "".getBytes("UTF-8");
+			byte[] binary = RandomUtils.nextBytes(20);
+			
+			log.info("SHA-256=" + Hex.encodeHexString(HashDemo.hash(HashDemo.ALGORITHM_SHA256,
+					binary)));
+			log.info("MD5=" + Hex.encodeHexString(HashDemo.hash(HashDemo.ALGORITHM_MD5, binary)));
+			log.info("CRC32=" + HashDemo.getCrc32(binary));
 		} catch (Exception ex) {
 			log.error(ex.getMessage(), ex);
 		}

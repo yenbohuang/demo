0e75602870092016e3b91ff53609d2e1d5e066ab
Yen-Bo Huang <yenbohuang@hotmail.com>
2016-02-24 10:32:38 +0800

    add Spring resource bundle demo

--- springDemo/src/main/java/org/yenbo/springDemo/config/WebConfig.java
+++ springDemo/src/main/java/org/yenbo/springDemo/config/WebConfig.java
@@ -1,8 +1,10 @@
 package org.yenbo.springDemo.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -31,5 +33,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages"); // messages.properties
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 }
--- springDemo/src/main/webapp/WEB-INF/views/home.jsp
+++ springDemo/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,7 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s" %>
+
 <%@page import="java.time.ZoneId"%>
 <%@page import="java.time.ZonedDateTime"%>
 
-<html><body>
-<h1>Spring MVC demo</h1>
-Server Time: <%=ZonedDateTime.now(ZoneId.of("UTC"))%>
-</body></html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+	<h1><s:message code="headline" /></h1>
+	<s:message code="servertime" />: <%=ZonedDateTime.now(ZoneId.of("UTC"))%>
+</body>
+</html>

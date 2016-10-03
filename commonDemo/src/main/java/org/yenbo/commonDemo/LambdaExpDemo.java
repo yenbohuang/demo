d7f3669ce24a952d57e58e10afc8acb056edeec9
Yen-Bo Huang <yenbohuang@hotmail.com>
2016-10-03 17:38:24 +0800

    add lambda expression demos

--- commonDemo/src/main/java/org/yenbo/commonDemo/LambdaExpDemo.java
+++ commonDemo/src/main/java/org/yenbo/commonDemo/LambdaExpDemo.java
@@ -7,13 +7,22 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * Sample codes from "Java in a Nutshell".
+ *
+ */
 public class LambdaExpDemo {
 
 	public static void main(String[] args) {
 		
 		listByFilter();
 		filterAndCollect();
+		mapToStringLength();
+		printByForEach();
+		sumByReduce();
+		flatternStreams();
 	}
 	
 	private static void listByFilter() {
@@ -60,7 +69,6 @@ public class LambdaExpDemo {
 		
 		String search = "tiger";
 		String[] input = {"tiger", "cat", "TIGER", "Tiger", "leopard"};
-		
 		List<String> cats = Arrays.asList(input);
 		
 		String tigers = cats.stream()
@@ -75,4 +83,86 @@ public class LambdaExpDemo {
 		
 		System.out.println(cats.stream().filter(combined).collect(Collectors.joining(", ")));
 	}
+	
+	private static void mapToStringLength() {
+		
+		System.out.println("-------- mapToStringLength() --------");
+		
+		String[] input = {"tiger", "cat", "TIGER", "Tiger", "leopard"};
+		List<String> cats = Arrays.asList(input);
+		
+		List<Integer> namesLength = cats.stream()
+				.map(String::length)
+				.collect(Collectors.toList());
+		
+		System.out.println(namesLength);
+	}
+	
+	private static void printByForEach() {
+		
+		System.out.println("-------- printByForEach() --------");
+		
+		String[] input = {"tiger", "cat", "TIGER", "Tiger", "leopard"};
+		List<String> cats = Arrays.asList(input);
+		
+		System.out.println("** Print by bound method reference: **");
+		cats.stream().forEach(System.out::println);
+		
+		System.out.println("** Print by anonymous method: **");
+		cats.stream().forEach(s -> System.out.println(s));
+	}
+	
+	private static void sumByReduce() {
+		
+		System.out.println("-------- sumByReduce() --------");
+		
+		double sumPrimes = (double) Stream.of(2, 3, 5, 7, 11, 13, 17, 19, 23)
+				.reduce(0,
+						(x, y) -> {
+							return x + y;
+						}
+					);
+		System.out.println("Sum of some primes: " + sumPrimes);
+	}
+	
+	private static void flatternStreams() {
+		
+		System.out.println("-------- flatternStreams() --------");
+		
+		String[] billyQuotes = {
+				"For Brutus is an honourable man",
+				"Give me your hands if we be friends and Robin shall restore amends",
+				"Misery acquaints a man with strange bedfellows"
+		};
+		List<String> quotes = Arrays.asList(billyQuotes);
+		
+		List<String> words = quotes.stream()
+				.flatMap(line -> Stream.of(line.split(" +")))
+				.collect(Collectors.toList());
+		
+		long wordCount = words.size();
+		
+		double avg = (double) words.stream()
+				.map(String::length)
+				.reduce(0, (x, y) -> {
+					return x + y;
+					}
+				) / wordCount;
+		
+		System.out.println("Avg. word length: " + avg);
+	}
+	
+	// TODO Collection::parallelStream
+	// TODO Collection::spliterator
+	// TODO Collection::removeIf
+	// TODO Map::forEach
+	// TODO Map:: computeIfAbsent
+	// TODO Map::computeIfPresent
+	// TODO Map::compute
+	// TODO Map::merge
+	// TODO Map::remove
+	// TODO Map::replace
+	// TODO Map::putIfAbsent
+	// TODO Map::getOrDefault
+	// TODO List::sort
 }

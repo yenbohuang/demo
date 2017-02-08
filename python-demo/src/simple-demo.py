744e489fb573d9d15c52453e7e82538a86940ff2
Yen-Bo Huang <yenbohuang@hotmail.com>
2017-02-08 16:00:14 +0800

    add python demo codes

---
+++ python-demo/src/simple-demo.py
@@ -0,0 +1,14 @@
+# simple print
+print(456)
+
+# assign
+a = 1
+print('a=', a)
+b = a
+print('b=a=', b)
+
+#type
+print('type of a', type(a))
+print('type of 1', type(1))
+print('type of 0.1', type(0.1))
+print('type of string', type('string'))

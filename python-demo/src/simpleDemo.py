14f1eee5a4dd4b89ad71aeda040214f880a11f0c
Yen-Bo Huang <yenbohuang@hotmail.com>
2017-02-10 11:51:54 +0800

    add python sample codes

--- python-demo/src/simpleDemo.py
+++ python-demo/src/simpleDemo.py
@@ -7,7 +7,7 @@ print('a=', a)
 b = a
 print('b=a=', b)
 
-#type
+# type of
 print('type of a', type(a))
 print('type of 1', type(1))
 print('type of 0.1', type(0.1))
@@ -19,6 +19,8 @@ print('integer division: 7 // 2 =', 7 // 2)
 print('7 % 2 =', 7 % 2)
 print('Get both truncated quotient and remainder at once by divmod(): ', divmod(7, 2))
 print('exponentiation: 3 ** 4 =', 3 ** 4)
+
+# bases
 print('Binary, 0b10 =', 0b10)
 print('Octal, 0o10 =', 0o10)
 print('Hex, 0x10 =', 0x10)
@@ -31,3 +33,18 @@ print('Convert 1.0e4 to int:', int(1.0e4))
 print('Convert string to int:', int('-23'))
 
 print('Convert string to float:', float('-23.56'))
+
+# string
+print('''line 1
+line 2
+line 3''')
+
+print('first ''second')
+print('first ' + 'second')
+
+print("'hello with single quote'")
+print('\'hello with single quote\'')
+
+print('Go ' * 4)
+offsetTest = 'abcd'
+print(offsetTest[0], offsetTest[-1])

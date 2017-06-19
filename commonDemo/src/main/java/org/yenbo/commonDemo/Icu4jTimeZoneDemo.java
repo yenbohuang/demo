fce59bf276302d759e646ce85c834aaf3ef5f201
Yen-Bo Huang <yenbohuang@hotmail.com>
2017-06-19 17:58:13 +0800

    Adjust ICU4J demo codes

--- commonDemo/src/main/java/org/yenbo/commonDemo/Icu4jTimeZoneDemo.java
+++ commonDemo/src/main/java/org/yenbo/commonDemo/Icu4jTimeZoneDemo.java
@@ -21,56 +21,47 @@ public class Icu4jTimeZoneDemo {
 	
 	public static void main(String[] args) {
 		
-		for (String country: Locale.getISOCountries()) {
+		compareSystemTimeZoneType();
+		firstTimezone();
+		checkExceptions();
+		canonicalizeTimezones();
+		canonicalTimezoneByCountry();
+	}
+	
+	private static void compareSystemTimeZoneType() {
 		
+		for (String country: Locale.getISOCountries()) {
+			
 			Set<String> anyTimeZones = TimeZone.getAvailableIDs(SystemTimeZoneType.ANY, country, null);
 			Set<String> canoicalTimeZones = TimeZone.getAvailableIDs(SystemTimeZoneType.CANONICAL, country, null);
 			Set<String> canoicalLocationTimeZones = TimeZone.getAvailableIDs(SystemTimeZoneType.CANONICAL_LOCATION, country, null);
 			String[] timezoneIds = TimeZone.getAvailableIDs(country);
 			
-			log.info("Country={}", country);
-			log.info("Number of ids: ANY={}, CANONICAL={}, CANONICAL_LOCATION={}, icuIds={}",
-					anyTimeZones.size(), canoicalTimeZones.size(), canoicalLocationTimeZones.size(), timezoneIds.length);
+			log.info("Country={}, Number of ids: ANY={}\t CANONICAL={}\t CANONICAL_LOCATION={}\t icuIds={}",
+					country, anyTimeZones.size(), canoicalTimeZones.size(), canoicalLocationTimeZones.size(), timezoneIds.length);
 			
 			if (canoicalLocationTimeZones.size() != canoicalTimeZones.size()) {
 				// The result: sizes are always match.
-				log.warn("Size of CANONICAL and CANONICAL_LOCATION not match");
+				log.warn("Size of CANONICAL and CANONICAL_LOCATION not match: country={}", country);
 			}
+		}
+	}
+	
+	private static void firstTimezone() {
+		
+		for (String country: Locale.getISOCountries()) {
 			
+			Set<String> canoicalTimeZones = TimeZone.getAvailableIDs(SystemTimeZoneType.CANONICAL, country, null);
+						
 			// select first timezone
 			if (canoicalTimeZones.isEmpty()) {
-				log.error("List is empty");
+				log.error("List is empty: country={}", country);
 			} else {
 				String[] zoneIdArray = canoicalTimeZones.toArray(new String[0]);
 				compareNormalized(zoneIdArray);
 				Arrays.sort(zoneIdArray);
-				log.info("Default timezone={}", ZoneId.of(zoneIdArray[0]).getId());
+				log.info("Country={}\t First timezone={}", country, ZoneId.of(zoneIdArray[0]).getId());
 			}
-			
-			// check of ZoneId throws exceptions
-			for (String id: canoicalLocationTimeZones) {
-				checkByZoneId(id);
-			}
-			
-			for (String id: canoicalTimeZones) {
-				checkByZoneId(id);
-			}
-			
-			for (String id: anyTimeZones) {
-				// Only this one throws exceptions
-				checkByZoneId(id);
-			}
-		}
-		
-		canonicalTimezones();
-	}
-	
-	private static void checkByZoneId(String zoneId) {
-		try {
-			ZoneId.of(zoneId).getId();
-		} catch (ZoneRulesException ex) {
-			log.info("ICU4J canonical ID for '{}' is '{}'", zoneId, TimeZone.getCanonicalID(zoneId));
-			log.error(ex.getMessage(), ex);
 		}
 	}
 	
@@ -86,15 +77,66 @@ public class Icu4jTimeZoneDemo {
 		}
 	}
 	
-	private static void canonicalTimezones() {
+	private static void checkExceptions() {
+		
+		for (String country: Locale.getISOCountries()) {
+			
+			checkByZoneId(TimeZone.getAvailableIDs(SystemTimeZoneType.CANONICAL_LOCATION, country, null));
+			checkByZoneId(TimeZone.getAvailableIDs(SystemTimeZoneType.CANONICAL, country, null));
+			
+			// Only this one throws exceptions
+			checkByZoneId(TimeZone.getAvailableIDs(SystemTimeZoneType.ANY, country, null));
+		}
+	}
+	
+	private static void checkByZoneId(Set<String> zoneSet) {
+		
+		for (String zoneId: zoneSet) {
+			try {
+				ZoneId.of(zoneId).getId();
+			} catch (ZoneRulesException ex) {
+				log.info("ICU4J canonical ID for '{}' is '{}'", zoneId, TimeZone.getCanonicalID(zoneId));
+				log.error(ex.getMessage(), ex);
+			}
+		}
+	}
+	
+	/**
+	 * This method generates a list which can be pasted into Excel easily for documentation usages.
+	 */
+	private static void canonicalizeTimezones() {
+		
+		System.out.format("Non-Canonical\tCanonical%n");
 		
 		for (String zoneId: TimeZone.getAvailableIDs()) {
 			
 			String canonical = TimeZone.getCanonicalID(zoneId);
 			
 			if (!zoneId.equals(canonical)) {
-				log.info("Non-canonical: {}, canonical: {}", zoneId, canonical);
-//				System.out.format("Non-canonical: %s, canonical: %s\n", zoneId, canonical);
+				System.out.format("%s\t%s%n", zoneId, canonical);
+			}
+		}
+	}
+	
+	/**
+	 * This method generates a list which can be pasted into Excel easily for documentation usages.
+	 */
+	private static void canonicalTimezoneByCountry() {
+		
+		for (String country: Locale.getISOCountries()) {
+			
+			System.out.format("%n%s (%s)%n", country, new Locale("en", country).getDisplayCountry());
+
+			Set<String> canoicalTimeZones = TimeZone.getAvailableIDs(SystemTimeZoneType.CANONICAL, country, null);
+			
+			if (canoicalTimeZones.isEmpty()) {
+				System.out.format("\t1. %s%n", TimeZone.GMT_ZONE.getID());
+			} else {
+				String[] zoneIdArray = canoicalTimeZones.toArray(new String[0]);
+				Arrays.sort(zoneIdArray);
+				for (int i = 0; i < zoneIdArray.length; i++) {
+					System.out.format("\t%d. %s%n", i + 1, zoneIdArray[i]);
+				}
 			}
 		}
 	}

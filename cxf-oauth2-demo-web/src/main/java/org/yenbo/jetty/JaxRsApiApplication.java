33e0fb122285ddee00be32835f19b6e6c49fc13c
yenbohuang <yenbohuang@localhost>
2017-09-04 12:16:06 +0800

    Add OAuth2 library with empty data provider

--- cxf-oauth2-demo-web/src/main/java/org/yenbo/jetty/JaxRsApiApplication.java
+++ cxf-oauth2-demo-web/src/main/java/org/yenbo/jetty/JaxRsApiApplication.java
@@ -3,6 +3,6 @@ package org.yenbo.jetty;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("api")
+@ApplicationPath("/oauth2")
 public class JaxRsApiApplication extends Application {
 }
--- cxf-oauth2-demo-web/src/main/java/org/yenbo/jetty/WebConfiguration.java
+++ cxf-oauth2-demo-web/src/main/java/org/yenbo/jetty/WebConfiguration.java
@@ -7,11 +7,14 @@ import javax.ws.rs.ext.RuntimeDelegate;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.rs.security.oauth2.provider.OAuthDataProvider;
+import org.apache.cxf.rs.security.oauth2.services.AccessTokenService;
+import org.apache.cxf.rs.security.oauth2.services.AuthorizationCodeGrantService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.yenbo.jetty.api.DemoService;
+import org.yenbo.jetty.oauth2.OAuthMemoryDataProvider;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
@@ -25,17 +28,23 @@ public class WebConfiguration {
     }
 	
 	@Bean
-	@DependsOn("cxf")
-    public Server jaxRsServer() {
+    public Server jaxRsServer(
+    		JaxRsApiApplication jaxRsApiApplication,
+    		JacksonJsonProvider jsonProvider,
+    		DemoService demoService,
+    		AuthorizationCodeGrantService authorizationCodeGrantService,
+    		AccessTokenService accessTokenService) {
+		
         JAXRSServerFactoryBean factory = RuntimeDelegate.getInstance().createEndpoint(
-        		jaxRsApiApplication(), JAXRSServerFactoryBean.class);
-        factory.setServiceBeans(Arrays.<Object>asList(
-        		demoService()
-        		));
-        factory.setProviders(Arrays.<Object>asList(
-        		jsonProvider()
-        		));
+        		jaxRsApiApplication, JAXRSServerFactoryBean.class);
+        factory.setProviders(Arrays.<Object>asList(jsonProvider));
         factory.setAddress(factory.getAddress());
+        
+        factory.setServiceBeans(Arrays.<Object>asList(
+        		demoService,
+        		authorizationCodeGrantService,
+        		accessTokenService
+        		));
         return factory.create();
     }
 	
@@ -52,5 +61,24 @@ public class WebConfiguration {
     @Bean 
     public DemoService demoService() {
         return new DemoService();
+    }
+    
+    @Bean
+    public OAuthDataProvider oauthDataProvider() {
+    	return new OAuthMemoryDataProvider();
+    }
+    
+    @Bean
+    public AccessTokenService accessTokenService(OAuthDataProvider oauthDataProvider) {
+    	AccessTokenService service = new AccessTokenService();
+    	service.setDataProvider(oauthDataProvider);
+    	return service;
+    }
+    
+    @Bean
+    public AuthorizationCodeGrantService authorizationCodeGrantService(OAuthDataProvider oauthDataProvider) {
+    	AuthorizationCodeGrantService service = new AuthorizationCodeGrantService();
+    	service.setDataProvider(oauthDataProvider);
+    	return service;
     }
 }

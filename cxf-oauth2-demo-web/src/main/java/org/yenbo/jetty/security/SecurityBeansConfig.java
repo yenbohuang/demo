8fdb00a191dd6a7584a1e1455a090c5ba7070057
Yen-Bo Huang <yenbohuang@gmail.com>
2017-10-12 11:00:13 +0800

    add requireCsrfProtectionMatcher

--- cxf-oauth2-demo-web/src/main/java/org/yenbo/jetty/security/SecurityBeansConfig.java
+++ cxf-oauth2-demo-web/src/main/java/org/yenbo/jetty/security/SecurityBeansConfig.java
@@ -1,5 +1,10 @@
 package org.yenbo.jetty.security;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.HttpMethod;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,6 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,11 +23,23 @@ public class SecurityBeansConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String LOGIN_FORM_URL = "/oauth2/login";
 	
+	private RequestMatcher requireCsrfProtectionMatcher() {
+	    
+	    List<RequestMatcher> requestMatchers = new ArrayList<>();
+	    requestMatchers.add(
+	        new RegexRequestMatcher("/oauth2/authenticate.*", HttpMethod.POST));
+	    requestMatchers.add(new RegexRequestMatcher(LOGIN_FORM_URL + ".*", HttpMethod.POST));
+	    
+	    return new OrRequestMatcher(requestMatchers);
+	  }
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http
-			.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.csrf()
+				.requireCsrfProtectionMatcher(requireCsrfProtectionMatcher())
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 			.and().exceptionHandling()
 				.authenticationEntryPoint(new DemoLoginUrlAuthenticationEntryPoint(LOGIN_FORM_URL))
 			.and().formLogin().loginPage(LOGIN_FORM_URL).permitAll()
